30e96a9d7eeb570fb07f04ba221c0880afe34af2
Valentyn <valaimg2218@gmail.com>
2020-09-09 03:07:51 +0300

    desktop

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
   dots: true,
   arrows: false
   });
+ $(function () {
+   var objt = $("div.person").length; // Считаем количество элементов с class="block"
+   if (objt > 3) {
+     $(".owl-carousel").owlCarousel({
+         items: 3,
+         loop: true,
+         nav: true,
+         dots: false
+     });
+   }
+ });
+ 
 });
