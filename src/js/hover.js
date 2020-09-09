9bc18d5495c25b1cf1c513cca00652fbc5062c82
Valentyn <valaimg2218@gmail.com>
2020-09-09 05:30:56 +0300

    animate

--- src/js/hover.js
+++
@@ -1,9 +0,0 @@
-$(".header__menu").hover(function () {
-  $("#Path_34").css({
-    'fill':'#A6CED0'
-  });
-}, function () {
-  $("#Path_34").css({
-    'fill':'none'
-  });
-});
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -14,5 +14,17 @@ $(document).ready(function(){
      });
    }
  });
- 
+new WOW().init();
+$(window).scroll(function() {
+
+    if ($(this).scrollTop() > 1){
+
+        $('header').addClass("sticky");
+
+    }  else{
+
+        $('header').removeClass("sticky");
+    }
+
+});
 });
