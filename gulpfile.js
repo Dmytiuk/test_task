e5578a40ab75137f3069b539682fc2ec27c8422c
Valentyn <valaimg2218@gmail.com>
2020-11-19 15:28:21 +0200

    first

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,67 @@
-'use strict'
+const 	gulp         = require('gulp'),
+		scss         = require('gulp-sass'),
+		autoprefixer = require('gulp-autoprefixer'),
+		sourcemaps   = require('gulp-sourcemaps'),
+		concat       = require('gulp-concat'),
+		uglify       = require('gulp-uglify'),
+		plumber		 = require('gulp-plumber');
+		babel        = require('gulp-babel');
+		csso         = require('gulp-csso');
 
-var gulp = require('gulp'),
-  scss = require('gulp-sass'),
-  sourcemaps = require('gulp-sourcemaps'),
-  babel = require('gulp-babel'),
-  concat = require('gulp-concat'),
-  plumber = require('gulp-plumber'),
-  imagemin = require('gulp-imagemin')
 
 var SASS_INCLUDE_PATHS = [
-  './node_modules/normalize-scss/sass/'
-]
+	'./node_modules/normalize-scss/sass'
+];
 
-function handleError (err) {
-  console.log(err.toString())
-  this.emit('end')
-}
+function handleError(err) {
+	console.log(err.toString());
+	this.emit('end');
+};
+
+
 
 gulp.task('styles', function () {
-  return gulp.src('./src/scss/main.scss')
-    .pipe(plumber({errorHandler: handleError}))
-    .pipe(sourcemaps.init())
-    .pipe(scss({outputStyle: 'compressed', includePaths: SASS_INCLUDE_PATHS}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/css'))
-})
+	return gulp.src('./src/scss/main.scss')
+		.pipe(plumber({ errorHandler: handleError }))
+		// .pipe(sourcemaps.init())
+		.pipe(scss({outputStyle: 'compact', includePaths: SASS_INCLUDE_PATHS}))
+		.pipe(autoprefixer({overrideBrowserslist: ['last 3 versions'], cascade: false}))
+		.pipe(csso())
+		// .pipe(sourcemaps.write())
+		.pipe(gulp.dest('./dist/css/'))
+});
 
-gulp.task('js', function () {
-  return gulp.src('./src/js/**/*.js')
-    .pipe(plumber({errorHandler: handleError}))
-    .pipe(sourcemaps.init())
-    .pipe(babel({compact: true}))
-    .pipe(concat('main.js'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/js'))
-})
+gulp.task('scripts', function() {
+	return gulp.src(['./src/js/**/*.js', '!./src/js/libs/**/*.js'])
+		.pipe(plumber({ errorHandler: handleError }))
+		// .pipe(sourcemaps.init())
+		.pipe(babel({
+			presets: ['@babel/env']
+		}))
+        .pipe(concat('main.js'))
+		.pipe(uglify())
+		// .pipe(sourcemaps.write())
+		.pipe(gulp.dest('./dist/js/'))
+});
 
-gulp.task('images', function () {
-  return gulp.src('./src/img/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/img'))
-})
+gulp.task('js-libs', function () {
+	return gulp.src('./src/js/libs/**/*.js')
+		.pipe(plumber({ errorHandler: handleError }))
+		.pipe(concat('libs.js'))
+		.pipe(gulp.dest('./dist/js/'))
+});
 
-gulp.task('watch', ['styles', 'js'], function () {
-  gulp.watch('./src/sсss/**/*.scss', ['styles'])
-  gulp.watch('./src/js/**/*.js', ['js'])
-})
+gulp.task('scss-libs', function () {
+	return gulp.src('./src/scss/libs/**/*.css')
+		.pipe(plumber({ errorHandler: handleError }))
+		.pipe(concat('libs.css'))
+		.pipe(gulp.dest('./dist/css/'))
+});
 
-gulp.task('default', ['styles', 'js', 'images'], function () {
+gulp.task('watch', function () {
+	gulp.watch(['src/scss/**/*.scss'], ['styles']);
+	gulp.watch(['src/js/**/*.js'], ['scripts']);
+	// gulp.watch('app/js/*.js').on("change", browserSync.reload);
+});
 
-})
+gulp.task('default', ['watch']);
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,35 +3,7 @@ $(document).ready(function(){
   dots: true,
   arrows: false
   });
- $(function () {
-   var objt = $("div.person").length;
-   if (objt > 3) {
-     $(".owl-carousel").owlCarousel({
-         items: 3,
-         loop: true,
-         nav: true,
-         dots: false
-     });
-   }
- });
+
 new WOW().init();
-$(window).scroll(function() {
-  if ($(window).width() > 1024) {
-    if ($(this).scrollTop() > 1){
-
-        $('header').addClass("sticky");
-
-    }  else{
-
-        $('header').removeClass("sticky");
-    }
-  }
-
-
-});
-           $('.menuBtn').click(function () {
-               $('.menuBtn').toggleClass('active');
-               $('.header__nav').toggleClass('opened');
-           });
 
 });
